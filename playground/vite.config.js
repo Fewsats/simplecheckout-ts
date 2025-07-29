f9545c8f5e9baf56c60e24bf9d08156697aa7de6
Pol Alvarez <pol.avms@gmail.com>
2025-07-29 11:11:16 +0200

    playground: running scripts

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
@@ -11,5 +12,10 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['@vgs/collect-js']
+  },
+  resolve: {
+    alias: {
+      '../dist/SmartCheckout.js': resolve(__dirname, '../src/SmartCheckout.ts')
+    }
   }
 }); 
